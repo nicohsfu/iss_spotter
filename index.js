fe3b6f5928d15c8183ce0de15be345c72cd2c234
nicohsfu <marcnicohernandez@gmail.com>
2022-09-29 16:03:45 -0700

    fetching rise times on given coordinates

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const { fetchMyIP, fetchCoordsByIP } = require('./iss');
+const { fetchMyIP, fetchCoordsByIP, fetchISSFlyOverTimes } = require('./iss');
 
 // fetchMyIP((error, ip) => {
 //   if (error) {
@@ -10,12 +10,39 @@ const { fetchMyIP, fetchCoordsByIP } = require('./iss');
 // });
 
 
-fetchCoordsByIP("66.183.57.76", (error, coordinates) => {
+// fetchCoordsByIP("66.183.57.76", (error, callback) => {
+//   if (error) {
+//     console.log("It didn't work!", error);
+//     return;
+//   }
+
+//   console.log('It worked! Returned coords:', callback);
+// });
+
+
+const exampleCoords = { latitude: '49.1044302', longitude: '-122.801094' };
+
+fetchISSFlyOverTimes(exampleCoords, (error, passTimes) => {
   if (error) {
-    console.log("It didn't work!", error);
+    console.log("It didn't work!" , error);
     return;
   }
 
-  console.log('It worked! Returned coordinates:', coordinates);
+  console.log('It worked! Returned flyover times:' , passTimes);
 });
 
+
+// chained fetchMyIP inside fetchCoordByIP
+// fetchCoordsByIP((error, ip) => {
+//   if (error) {
+//     console.log("It didn't work!", error);
+//     return;
+//   }
+
+//   console.log('It worked! Returned IP:', ip);
+// });
+
+
+// lat then long
+// 49.1044302
+// -122.801094
